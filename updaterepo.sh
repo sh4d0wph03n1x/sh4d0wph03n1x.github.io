5d285f64dd9a72d40e07631a97252354b4bf5730
sh4d0wph03n1x <zacharysnell@outlook.com>
2021-05-25 15:36:36 -0500

    Updating update script

--- updaterepo.sh
+++ updaterepo.sh
@@ -8,5 +8,6 @@ bzip2 ~/sh4d0wph03n1x.github.io/Packages
 dpkg-scanpackages -m ~/sh4d0wph03n1x.github.io/debs > ~/sh4d0wph03n1x.github.io/Packages
 
 git add *
-git commit -m "Add debs"
+read -p "Commit message: " cms
+git commit -m "$cms"
 git push -u origin master
