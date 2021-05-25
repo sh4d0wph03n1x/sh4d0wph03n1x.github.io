166ead665e389446ca6a40fe2afa1969961178b0
sh4d0wph03n1x <zacharysnell@outlook.com>
2021-05-25 15:34:50 -0500

    Add debs

--- updaterepo.sh
+++ updaterepo.sh
@@ -7,6 +7,6 @@ bzip2 ~/sh4d0wph03n1x.github.io/Packages
 
 dpkg-scanpackages -m ~/sh4d0wph03n1x.github.io/debs > ~/sh4d0wph03n1x.github.io/Packages
 
-git add ~/sh4d0wph03n1x.github.io/*
-git commit ~/sh4d0wph03n1x.github.io -m "Add debs"
-git push ~/sh4d0wph03n1x.github.io -u origin master
+git add *
+git commit -m "Add debs"
+git push -u origin master
